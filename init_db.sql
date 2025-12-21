6b82da1bc7f13d976af8fa997180c6d449464e81
hyperf0rm <drk2701@gmail.com>
2025-12-21 13:44:12 +0300

    add docker-compose.prod.yml

--- init_db.sql
+++ init_db.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS light_bot;
 
 CREATE TABLE light_bot.users (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id bigint NOT NULL UNIQUE,
+    user_id bigint NOT NULL CONSTRAINT user_id_unique UNIQUE,
     username varchar(32),
     last_message_hash char(32),
     created_at timestamptz DEFAULT now() NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE light_bot.addresses (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id bigint NOT NULL REFERENCES light_bot.users(user_id) ON DELETE CASCADE,
     address varchar(255) NOT NULL,
-    created_at timestamptz DEFAULT now() NOT NULL
+    created_at timestamptz DEFAULT now() NOT NULL,
     UNIQUE (user_id, address)
 );
 
